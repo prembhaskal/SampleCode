c55cc532f187659637d97d9e47cf595e21effefd
prembhaskal <pbhaskal@gmail.com>
2014-06-03 23:21:31 +0530

    started java 8 practice.

---
+++ java8_sample/src/com/practice/stream/StreamPractice.java
@@ -0,0 +1,23 @@
+package com.practice.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class StreamPractice {
+
+	public static void main(String[] args) {
+		StreamPractice streamPractice = new StreamPractice();
+		streamPractice.getLongestLine();
+	}
+
+	private void getLongestLine() {
+		String[] list = new String[]{"stringa", "stringabbbbbaa", "stringaaaaaa"};
+
+		List<String> stringList = Arrays.asList(list);
+		String longestWord = stringList.stream().reduce((a,b) -> a.length() > b.length() ? a : b).orElse("");
+		System.out.println("longest word is " + longestWord);
+
+		stringList.stream().map(w -> w.length()).forEach(s -> System.out.println(s));
+	}
+}
